e7a76e093262c52fc4d899f64f8132932b5904e0
koonukaame <marialezhebokova@yandex.ru>
2024-10-27 19:28:35 +0300

    feat: solve removeFirstOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -152,8 +152,15 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const valueIndex = str.indexOf(value);
+  if (valueIndex === -1) {
+    return str;
+  }
+  return `${str.substring(0, valueIndex)}${str.substring(
+    valueIndex + value.length,
+    str.length
+  )}`;
 }
 
 /**
