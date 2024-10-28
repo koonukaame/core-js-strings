94d61368635b2aa5ab68e38cd8ea066923f0225d
koonukaame <marialezhebokova@yandex.ru>
2024-10-28 15:40:44 +0300

    feat: solve reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -379,8 +379,11 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const newStr = str.split(' ').map((el) => {
+    return [...el].reverse().join('');
+  });
+  return newStr.join(' ');
 }
 
 /**
